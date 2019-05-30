8666b8f12d3bc683962de5acdde94962b6ba30ea
asiervelasco <asiervelasco@opendeusto.es>
2019-05-30 21:20:48 +0200

    actualizacion buena revisada

--- ProyectoProgram/src/Excepciones/clsListaVacia.java
+++ ProyectoProgram/src/Excepciones/clsListaVacia.java
@@ -11,7 +11,7 @@ public class clsListaVacia extends Exception{
 	
 	private static final long serialVersionUID = 1L;
 
-	private final String MENSAJE = "La lista de vehiculos se encuentra vacía, por favor, introduce un libro";	
+	private final String MENSAJE = "La lista de vehiculos se encuentra vacía, por favor, introduce un vehículo";	
 	@Override
 	public String getMessage() {
 		// TODO Auto-generated method stub
